130506756961e6d556582ee1d1599aaa57f3ab56
joel <joel@deb.casa.org>
2023-12-02 11:49:50 -0500

    Add only table_options method

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-from utils.table_options import table
-from rich.console import Console
-from rich import print
-import subprocess
-
-console = Console()
+from utils.tables import _table_options
 
 
 if __name__ == "__main__":
-    console.print(table)
-    print("[orange3] Whats option you choose?")
-    option_number: int = int(input("» "))
-    print("[orange3] Audio or video?")
-    option_filter: str = input("» ")
-    if option_number == 1 and option_filter == "audio": subprocess.run(["python3", "scripts/filter.py", "--audio"])
+    _table_options()
