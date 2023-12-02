fcf4a4384f15cd9642b7c0750ac304a80e7158d2
joel <joel@deb.casa.org>
2023-12-02 11:48:29 -0500

    Add functions of the tables

---
+++ utils/tables.py
@@ -0,0 +1,31 @@
+from utils.table_formats import table_formats
+from utils.table_options import table
+from rich.console import Console
+from rich import print
+import subprocess
+import os
+
+console = Console()
+
+
+def _table_options():
+    console.print(table)
+    print("[orange3] Whats option you choose?")
+    option_number: int = int(input("» "))
+    if option_number == 3:
+        os.abort()
+    _table_formats(option_number)
+
+
+def _table_formats(option_number):
+    console.print(table_formats)
+    print("[orange3] Audio or video?")
+    option_filter: int = int(input("» "))
+
+    if option_number == 1 and option_filter == 1:
+        subprocess.run(["python3", "scr/filter.py", "--audio"])
+        _table_options()
+    elif option_number == 1 and option_filter == 2:
+        subprocess.run(["python3", "scr/filter.py", "--video"])
+        _table_options()
+
