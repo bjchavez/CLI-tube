4c164972d8ebe4762c303d89b67a5eff51a764ef
joel <joel@deb.casa.org>
2023-12-03 11:19:06 -0500

    Fix a grammatical error in the options table rows

--- utils/table_options.py
+++ utils/table_options.py
@@ -7,8 +7,8 @@ table = Table(title="Ty-Tube", show_lines=True, box=box.ROUNDED)
 table.add_column("Option", justify="center", style="#fab387")
 table.add_column("Description", justify="left", style="#94e2d5")
 
-table.add_row("1", "Filters a link from Youtube, you can choose between audio or video.")
-table.add_row("2", "Downloads an audio or video from Youtube.")
+table.add_row("1", "Filter a YouTube link, you can choose between audio or video.")
+table.add_row("2", "Download an audio or video from Youtube.")
 table.add_row("3", "Exit app.")
 
 console = Console()
