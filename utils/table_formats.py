9c3f403e6fff953a6cfc02b4f5644cbf5e86dc70
joel <joel@deb.casa.org>
2023-12-03 08:28:17 -0500

    Add styles to tables columns

--- utils/table_formats.py
+++ utils/table_formats.py
@@ -4,8 +4,8 @@ from rich import box
 
 table_formats = Table(title="Formats", show_lines=True, box=box.ROUNDED)
 
-table_formats.add_column("Option", justify="center", style="orange3")
-table_formats.add_column("Description", justify="left")
+table_formats.add_column("Option", justify="center", style="#fab387")
+table_formats.add_column("Description", justify="left", style="#94e2d5")
 
 table_formats.add_row("1", "For filter to audio.")
 table_formats.add_row("2", "For filter to video.")
--- utils/table_options.py
+++ utils/table_options.py
@@ -4,8 +4,8 @@ from rich import box
 
 table = Table(title="Ty-Tube", show_lines=True, box=box.ROUNDED)
 
-table.add_column("Option", justify="center", style="orange3")
-table.add_column("Description", justify="left")
+table.add_column("Option", justify="center", style="#fab387")
+table.add_column("Description", justify="left", style="#94e2d5")
 
 table.add_row("1", "Filters a link from Youtube, you can choose between audio or video.")
 table.add_row("2", "Downloads a audio or video from Youtube.")
